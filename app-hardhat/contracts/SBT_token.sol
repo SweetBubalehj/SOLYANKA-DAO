30103996c2c3b492d914bddb0c001aa5b8292cf2
Kirill Ruchkin <lirikk@MacBook-Pro-Kirill.local>
2023-04-12 15:16:16 +0300

    Change SBT

--- app-hardhat/contracts/SBT_token.sol
+++ app-hardhat/contracts/SBT_token.sol
@@ -1,23 +1,60 @@
 // SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
 
-pragma solidity ^0.8.9;
-
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
-contract Token is ERC20 {
-    address public owner;
-
-    constructor (uint totalSupply) ERC20("SBToken", "SBT") {
-        owner = msg.sender;
-        _mint(msg.sender, totalSupply);
+contract SBT {
+    struct Soul {
+        string identity;
+        string name;
+        uint age;
+        bool KYC;
     }
 
+    mapping(address => Soul) private souls;
+
+    string public name;
+    string public title;
+    address public owner;
+    bytes32 zeroHash = 0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470;
+
     modifier OnlyOwner {
-        require(msg.sender == owner, "Not owner");
+        require(owner == msg.sender, "Not Owner");
         _;
     }
 
-    function mint(address addr, uint amount) public OnlyOwner {
-        _mint(addr, amount);
+    constructor (string memory _name, string memory _title) {
+        name = _name;
+        title = _title;
+        owner = msg.sender;
+    }
+
+    function createSoul (address addrSoul, string calldata _name, uint _age) public OnlyOwner {
+        require(keccak256(bytes(souls[addrSoul].identity)) == zeroHash, "Soul already exists");
+        souls[addrSoul].identity = "1";
+        souls[addrSoul].name = _name;
+        souls[addrSoul].age = _age;
+        souls[addrSoul].KYC = false;
+    }
+
+    function deleteSoul (address addrSoul) public OnlyOwner {
+        require(keccak256(bytes(souls[addrSoul].identity)) != zeroHash, "Soul does not exist");
+        delete souls[addrSoul];
+    }
+
+    function turnKYC(address addrSoul, bool _kyc) public OnlyOwner {
+        souls[addrSoul].KYC = _kyc;
+    }
+
+    function updateDataSoul (address addrSoul, string calldata _name, uint _age) public OnlyOwner {
+        require(keccak256(bytes(souls[addrSoul].identity)) != zeroHash, "Soul does not exist");
+        souls[addrSoul].name = _name;
+        souls[addrSoul].age = _age;
+    }
+
+    function checkKYC(address addrSoul) public view returns(bool){
+        return souls[addrSoul].KYC ? true : false;
+    }
+
+    function getSoul(address addrSoul) public view returns(Soul memory){
+        return souls[addrSoul];
     }
 }
